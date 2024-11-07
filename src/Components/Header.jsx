1a91d8bed8df5ed2c37a33f70a9a77fa60e70e9d
Zain <mdzainkamal@gmail.com>
2024-11-07 23:33:18 +0530

    Added CarBazaar Header with style.

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import '../Styles/Header.css'
+
+const Header = () => {
+    return (
+        <>
+            <h1 className="heading"><p className='mainheader'>Car Bazaar</p></h1>
+        </>
+    )
+}
+
+export default Header;
